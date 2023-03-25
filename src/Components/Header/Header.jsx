3d5c5c7ddcc6ef6d6b52e94fa9161c5ff332c62b
andremacfelipe <andremacielfelipe@gmail.com>
2023-03-25 04:06:12 -0300

    Link added to Register/Login button and Logo

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./style.css"
 
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 import Logo from "../Logo/Logo";
 import ShoppingKart from "../MaterialSymbolsRounded/ShoppingKart";
@@ -9,23 +9,33 @@ import MenuIcon from "../MaterialSymbolsRounded/MenuIcon";
 import AccountCircle from "../MaterialSymbolsRounded/AccountCircle";
 import SearchBar from "../SearchBar/SearchBar";
 
+
+
+
+
 const Header = () => {
     return (
         <>
             <header className="Header">
                 <MenuIcon />
                 <div className="headerDBox">
-                    <Logo />
+                    <Link to="/">
+                        <Logo />
+                    </Link>
                 </div>
                 <SearchBar />
 
                 <div className="HeaderRegisterAndLoginContainer">
                     <AccountCircle />
-                    <button>
-                        Login
-                    </button>
+                    <Link to="/login" className="loginLink">
+                        <button>
+                            Login
+                        </button>
+                    </Link>
                     <span className="headerText">or</span>
-                    <button>Register</button>
+                    <Link to="/register">
+                        <button>Register</button>
+                    </Link>
                 </div>
                 <div className="HeaderMisc">
                     <ShoppingKart />
