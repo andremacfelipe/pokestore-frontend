6c51d53e0aa9d8e008baf14aed6e5d5d4b2520fa
andremacfelipe <andremacielfelipe@gmail.com>
2023-04-02 13:34:44 -0300

    AuthContext created

---
+++ src/Contexts/AuthContext/AuthContext.jsx
@@ -0,0 +1,17 @@
+import React, {createContext} from "react";
+
+const AuthContext = createContext({})
+
+export const AuthProvider = ({children}) => {
+
+    
+
+
+
+    return(
+        <AuthContext.Provider>
+
+
+        </AuthContext.Provider>
+    )
+}
