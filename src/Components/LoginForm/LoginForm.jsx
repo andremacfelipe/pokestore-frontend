e77077a88a497b0bda391bc403caf736bda0d3cc
andremacfelipe <andremacielfelipe@gmail.com>
2023-04-02 11:58:50 -0300

    LoginForm created

---
+++ src/Components/LoginForm/LoginForm.jsx
@@ -0,0 +1,15 @@
+import "./style.css"
+import React from "react"
+
+
+const LoginForm = () => {
+    return (
+        <form className="LoginForm">
+
+
+        </form>
+    )
+}
+
+export default LoginForm
+
