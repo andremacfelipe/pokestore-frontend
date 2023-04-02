aeb95923c2905b66ed834f601d2c808dcac93652
andremacfelipe <andremacielfelipe@gmail.com>
2023-04-02 12:43:05 -0300

    LoginForm added to register page

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -1,11 +1,44 @@
 import "./style.css"
-import React from "react"
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
 
+import Logo from "../Logo/Logo"
 
 const LoginForm = () => {
+    const [loginUserInputs,setLoginUserInputs] = useState({email:"",password:""})
+
+    const handleChange = (e) => {
+        const fieldName = e.target.name
+        const fieldValue = e.target.value
+
+        setLoginUserInputs({
+            ...loginUserInputs,
+            [fieldName]: fieldValue
+        })
+    }
+
+
+
     return (
         <form className="LoginForm">
+            <div className="FormBox">
+                <Logo />
+                <h1>Login</h1>
+            </div>
+            <input type="email" name="email" value={loginUserInputs.email} required className="LoginFormInput" id="emailInput" 
+                placeholder="Email..."
+                onChange={handleChange}
+            />
+            <input type="password" required name="password" value={loginUserInputs.password}
+                className="LoginFormInput" id="passwordInput" placeholder="Password..."
+                onChange={handleChange}
+                
+            />
 
+            <button type="submit" className="LoginButton">Login</button>
+            <span className="dontHaveAnAccount">
+                Don't have an account yet? <Link className="RegisterButtonLink" to="/register">Register</Link>
+            </span>
 
         </form>
     )
--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -1,12 +1,12 @@
 import "./style.css"
 import React from "react"
-
+import LoginForm from "../../Components/LoginForm/LoginForm"
 
 const LoginPage = () => {
     return(
         <main className="LoginPage">
-
-        </main>
+            <LoginForm />
+        </main> 
     )
 }
 
