d7613da16a08fd0e9c743e615bbe2c29f2afe837
andremacfelipe <andremacielfelipe@gmail.com>
2023-04-02 11:56:16 -0300

    LoginPage created

---
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,13 @@
+import "./style.css"
+import React from "react"
+
+
+const LoginPage = () => {
+    return(
+        <main className="LoginPage">
+
+        </main>
+    )
+}
+
+export default LoginPage
