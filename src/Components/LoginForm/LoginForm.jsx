14556c02c1218a53276896400e21d40211f296fd
andremacfelipe <andremacielfelipe@gmail.com>
2023-05-04 18:40:03 -0300

    LoginForm validation refactored

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -1,6 +1,6 @@
 import "./style.css"
 import React, { useContext, useState } from "react"
-import { Link,useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 import Logo from "../Logo/Logo"
 
@@ -10,12 +10,13 @@ import { login } from "../../services/auth/login"
 import { validateSession } from "../../services/auth/validateSession"
 
 const LoginForm = () => {
-    const [loginUserInputs,setLoginUserInputs] = useState({email:"",password:""})
-    const [loginError,setLoginError] = useState(null)
+    const [loginUserInputs, setLoginUserInputs] = useState({ email: "", password: "" })
+    const [loginError, setLoginError] = useState(null)
+    const [loginLoading, setLoginLoading] = useState(false)
 
     const navigate = useNavigate()
 
-    const {setUserData} = useContext(AuthContext)
+    const { setUserData, logout } = useContext(AuthContext)
 
     const handleChange = (e) => {
         const fieldName = e.target.name
@@ -29,33 +30,37 @@ const LoginForm = () => {
 
     const handleLoginSubmit = () => {
 
-        if (loginUserInputs.email && loginUserInputs.password){
+        if (loginUserInputs.email && loginUserInputs.password) {
             login(loginUserInputs)
-            .then( async res => {
-                if (!res.ok){
-                    console.log(res.response)
-                    setLoginError(res.response.message)
-                    setUserData(null)
-                    sessionStorage.clear()
-                    throw Error()
-                }else{
-                    sessionStorage.setItem("USER_TOKEN",res.response.USER_TOKEN)
-                    try {
-                        const validateSessionResponse = await validateSession()
-                        if (!validateSessionResponse.ok){
-                            sessionStorage.clear()
-                            setUserData(null)
-                        }else{
-                            setUserData(validateSessionResponse.response)
-                            navigate("/")
-                        }
-
-
-                    } catch (err) {
-                        console.log(err)
+                .then(async res => {
+                    setLoginLoading(true)
+                    if (!res.ok) {
+                        setLoginError(res.response.message)
+                        setUserData(null)
+                        sessionStorage.clear()
+                        throw Error()
+                    } else {
+                        sessionStorage.setItem("USER_TOKEN", res.response.USER_TOKEN)
+                        return res.response.USER_TOKEN
                     }
-                }
-            } ).catch(err => {})
+                }).then(async (token) => {
+                    await validateSession(token)
+                        .then((res) => {
+                            if (!res.ok) {
+                                logout()
+                            } else {
+                                setUserData(res.response)
+                                navigate("/")
+                            }
+                        }).catch(err => {
+                            //Token error 
+                        })
+                }).catch(err => {
+                    //Login error
+                }).finally(() => {
+                    //Add loginLoading in future
+                    setLoginLoading(false)
+                })
         }
 
 
@@ -70,20 +75,20 @@ const LoginForm = () => {
                 <Logo />
                 <h1>Login</h1>
             </div>
-            <input type="email" name="email" value={loginUserInputs.email} required className="LoginFormInput" id="emailInput" 
+            <input type="email" name="email" value={loginUserInputs.email} required className="LoginFormInput" id="emailInput"
                 placeholder="Email..."
                 onChange={handleChange}
             />
             <input type="password" required name="password" value={loginUserInputs.password}
                 className="LoginFormInput" id="passwordInput" placeholder="Password..."
                 onChange={handleChange}
-                
+
             />
             <span className="LoginErrorMessage">
                 {loginError ? loginError : ""}
             </span>
 
-            <button type="submit" className="LoginButton" onClick={ handleLoginSubmit } >Login</button>
+            <button type="submit" className="LoginButton" onClick={handleLoginSubmit} >Login</button>
             <span className="dontHaveAnAccount">
                 Don't have an account yet? <Link className="RegisterButtonLink" to="/register">Register</Link>
             </span>
