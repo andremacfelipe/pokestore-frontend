fc368689bbea86e2ad6993014c6fe46fe5738eaf
andremacfelipe <andremacielfelipe@gmail.com>
2023-05-04 23:20:31 -0300

    SellListings Table Header added

--- src/Components/SellListings/SellListings.jsx
+++ src/Components/SellListings/SellListings.jsx
@@ -1,11 +1,25 @@
 import "./style.css"
 import React from "react"
 
+import SellListingCard from "../SellListingCard/SellListingCard"
 
 const SellListings = () => {
     return (
         <div className="SellListings">
 
+            <div className="SellListingsTableRows">
+                <div className="SellListingsHeader">
+                    <div className="SellListingsItemNameColumn">
+                        NAME
+                    </div>
+                    <div className="SellListingsItemAmountColumn">
+                        AMOUNT
+                    </div>
+                    <div className="SellListingsItemPriceColumn">
+                        PRICE
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
