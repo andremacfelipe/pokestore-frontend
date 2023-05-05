9b5ac6b8ad7a09b5ce0dbf206ce9f2d66f2ab4e6
andremacfelipe <andremacielfelipe@gmail.com>
2023-05-04 21:07:29 -0300

    SellListingCard created

---
+++ src/Components/SellListingCard/SellListingCard.jsx
@@ -0,0 +1,12 @@
+import "./style.css"
+import React from "react"
+
+
+const SellListingCard = () => {
+    return (
+        <div className="SellListingCard">
+            
+        </div>
+    )
+}
+export default SellListingCard
