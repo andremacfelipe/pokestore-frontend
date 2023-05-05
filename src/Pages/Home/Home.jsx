0b6548b04ac68c3fa34d3865bd8c523d52bd40ca
andremacfelipe <andremacielfelipe@gmail.com>
2023-05-04 23:22:23 -0300

    SellListings added to Home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import "./style.css"
 import React from "react";
 
+import SellListings from "../../Components/SellListings/SellListings";
+
 
 const Home = () => {
     return (
         <main className="HomePage">
+            <SellListings />
         </main>
     )
 }
