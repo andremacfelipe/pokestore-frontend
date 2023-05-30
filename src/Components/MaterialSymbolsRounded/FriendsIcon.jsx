f1278ddad3ae2a63b527a9fa6196161578ac75a7
andremacfelipe <andremacielfelipe@gmail.com>
2023-05-30 18:54:32 -0300

    FriendsIcon added

---
+++ src/Components/MaterialSymbolsRounded/FriendsIcon.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FriendsIcon = () => {
+    return (
+        <span className="material-symbols-rounded FriendsIcon notranslate">
+            group
+        </span>
+    )
+}
+
+export default FriendsIcon
