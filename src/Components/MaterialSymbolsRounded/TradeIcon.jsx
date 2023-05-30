e13711666fde09a6ef284db3c578aaf05710b98c
andremacfelipe <andremacielfelipe@gmail.com>
2023-05-30 18:57:35 -0300

    Trade Icon added

---
+++ src/Components/MaterialSymbolsRounded/TradeIcon.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TradeIcon = () => {
+    return (
+        <span className="material-symbols-rounded TradeIcon notranslate">
+            handshake
+        </span>
+    )
+}
+
+export default TradeIcon
