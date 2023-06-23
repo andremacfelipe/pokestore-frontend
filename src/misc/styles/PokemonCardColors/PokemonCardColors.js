1b16b0960b7dc4a4db5c70117cd4016173336032
andremacfelipe <andremacielfelipe@gmail.com>
2023-06-23 04:08:02 -0300

    PokemonCardColors added

---
+++ src/misc/styles/PokemonCardColors/PokemonCardColors.js
@@ -0,0 +1,42 @@
+const PokemonCardTypesColors = {
+    bug: "#92C12Dcc",
+    dark: "#5F5A6Dcc",
+    dragon: "#0681BCcc",
+    electric: "#F6D852cc",
+    fairy: "#F09DE3cc",
+    fighting: "#D64261cc",
+    fire: "#FFA152cc",
+    flying: "#97B1E3cc",
+    ghost: "#626DBDcc",
+    grass: "#60BA56cc",
+    ground: "#D97948cc",
+    ice: "#75CFC1cc",
+    normal: "#9099A1cc",
+    poison: "#A766C8cc",
+    psychic: "#F87379cc",
+    rock: "#C8B88Bcc",
+    steel: "#548FA0cc",
+    water: "#51DCE2cc"
+}
+const PokemonCardTypesBackgroundColor = {
+    bug: "#92C12D",
+    dark: "#5F5A6D",
+    dragon: "#0681Bc",
+    electric: "#F6D852",
+    fairy: "#F09DE3",
+    fighting: "#D64261",
+    fire: "#FFA152",
+    flying: "#97B1E3",
+    ghost: "#626DBD",
+    grass: "#60BA56",
+    ground: "#D97948",
+    ice: "#75CFC1",
+    normal: "#9099A1",
+    poison: "#A766C8",
+    psychic: "#F87379",
+    rock: "#C8B88B",
+    steel: "#548FA0",
+    water: "#51DCE2"
+}
+
+export { PokemonCardTypesBackgroundColor, PokemonCardTypesColors }
