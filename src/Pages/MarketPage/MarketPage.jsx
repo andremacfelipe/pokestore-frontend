3e9e00945fb4665b6f8d5f1c06f5ad5f9f3c62b2
andremacfelipe <andremacielfelipe@gmail.com>
2023-06-27 16:19:19 -0300

    MarketPage added

---
+++ src/Pages/MarketPage/MarketPage.jsx
@@ -0,0 +1,15 @@
+import "./style.css"
+import React from "react";
+
+import SellListings from "../../Components/SellListings/SellListings";
+
+
+const MarketPage = () => {
+    return (
+        <main className="MarketPage">
+            <SellListings />
+        </main>
+    )
+}
+
+export default MarketPage
