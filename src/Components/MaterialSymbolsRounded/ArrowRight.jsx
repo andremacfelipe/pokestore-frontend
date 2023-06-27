a15224d0d23ebb9de3cf599960bf9922cf6a1615
andremacfelipe <andremacielfelipe@gmail.com>
2023-06-27 08:30:33 -0300

    ArrowRight added

---
+++ src/Components/MaterialSymbolsRounded/ArrowRight.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const ArrowRight = () => {
+    return (
+        <span className="material-symbols-rounded ArrowRight notranslate">
+            arrow_right
+        </span>
+    )
+}
+
+
+export default ArrowRight
