14e525356d8cb5cf82e67e77cb9a19c4efcf56f9
andremacfelipe <andremacielfelipe@gmail.com>
2023-06-29 00:42:52 -0300

    Market Page title changed

--- src/Pages/MarketPage/MarketPage.jsx
+++ src/Pages/MarketPage/MarketPage.jsx
@@ -1,10 +1,15 @@
 import "./style.css"
-import React from "react";
+import React, { useEffect } from "react";
 
 import SellListings from "../../Components/SellListings/SellListings";
 
 
 const MarketPage = () => {
+
+    useEffect(() => {
+        document.title = "Market"
+    },[])
+
     return (
         <main className="MarketPage">
             <SellListings />
