e0c41cdc23368f7b30d53e0e221cfc19310cdfef
andremacfelipe <andremacielfelipe@gmail.com>
2023-07-20 18:30:39 -0300

    added: item prop

--- src/Components/SellListingCard/SellListingCard.jsx
+++ src/Components/SellListingCard/SellListingCard.jsx
@@ -1,25 +1,36 @@
 import "./style.css"
-import React from "react"
+import React, { useEffect } from "react"
 
+import { PokemonCardTypesBackgroundColor,PokemonCardTypesColors } from "../../misc/styles/PokemonCardColors/PokemonCardColors"
 
-const SellListingCard = ({ className }) => {
+const SellListingCard = ({ item, className }) => {
     return (
         <div className="SellListingCard">
 
             <div className="SellListingsItemNameColumn">
-                <div className="SellListingCardItemPic">
-
+                <div className="SellListingCardItemPic" 
+                    style={{
+                        boxShadow:`1px 1px 1px ${PokemonCardTypesColors[item.info[0].type.name]}, -1px -1px 1px ${PokemonCardTypesColors[item.info[0].type.name]} `,
+                        outline:`1px solid ${PokemonCardTypesColors[item.info[0].type.name]}`
+                    }}
+                >
+                    <img src={item?.itemPic} alt={item?.itemName} />
                 </div>
-                <div className="SellListingCardItemNameText">
-                    Pikachu the goat
+                <div className="SellListingCardItemNameText"
+                    style={{
+                        color:PokemonCardTypesBackgroundColor[item.info[0].type.name],
+                        
+                    }}
+                >
+                    {item?.itemName}
                 </div>
             </div>
 
             <div className="SellListingsItemAmountColumn">
-                1.000
+                {item?.amount}
             </div>
             <div className="SellListingsItemPriceColumn">
-                49.99
+                {item?.price.toFixed(2)}
             </div>
         </div>
     )
